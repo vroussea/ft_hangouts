ece302c5c3d0e9849f406fdd8305c77165a2f168
vroussea <59ichiigo@gmail.com>
2018-02-26 19:23:38 +0100

    contact edit page now should work soon

--- app/src/main/java/com/vroussea/myapplication/activities/ContactEdit.java
+++ app/src/main/java/com/vroussea/myapplication/activities/ContactEdit.java
@@ -11,6 +11,6 @@ public class ContactEdit extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_edit);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/vroussea/myapplication/activities/ContactsActivity.java
+++ app/src/main/java/com/vroussea/myapplication/activities/ContactsActivity.java
@@ -1,13 +1,16 @@
 package com.vroussea.myapplication.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import com.vroussea.myapplication.R;
@@ -18,6 +21,8 @@ import com.vroussea.myapplication.contact.ContactDao;
 import com.vroussea.myapplication.contact.ContactDatabase;
 
 import java.util.List;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class ContactsActivity extends AppCompatActivity {
     ContactDao mDao;
@@ -72,4 +77,25 @@ public class ContactsActivity extends AppCompatActivity {
             });
         }).start();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add_contact) {
+            Intent intent = new Intent(this, ContactEdit.class);
+            startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
