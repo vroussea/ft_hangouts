45effa57dde21b490ccab4b99f696c06859629ed
vroussea <59ichiigo@gmail.com>
2018-02-28 18:48:45 +0100

    i can now add all the contacts i want ! :D

--- app/src/main/java/com/vroussea/myapplication/activities/ContactEdit.java
+++ app/src/main/java/com/vroussea/myapplication/activities/ContactEdit.java
@@ -1,16 +1,60 @@
 package com.vroussea.myapplication.activities;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.vroussea.myapplication.R;
+import com.vroussea.myapplication.contact.Contact;
+import com.vroussea.myapplication.contact.ContactBuilder;
+import com.vroussea.myapplication.contact.ContactDao;
+import com.vroussea.myapplication.contact.ContactDatabase;
 
-public class ContactEdit extends Activity {
+public class ContactEdit extends AppCompatActivity {
+
+    ContactDao mDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_edit);
-        //getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
+    public void onSubmitContact(View view) {
+        String firstName= ((EditText)findViewById(R.id.first_name)).getText().toString();
+        String lastName = ((EditText)findViewById(R.id.last_name)).getText().toString();
+        String nickname = ((EditText)findViewById(R.id.nickname)).getText().toString();
+        String phoneNumber = ((EditText)findViewById(R.id.phone_number)).getText().toString();
+        String eMail = ((EditText)findViewById(R.id.eMail)).getText().toString();
+
+        if (!phoneNumber.equals("") && !firstName.equals("")) {
+            Contact newContact = ContactBuilder.aContact()
+                    .withFirstName(firstName)
+                    .withLastName(lastName)
+                    .withNickname(nickname)
+                    .withPhoneNumber(phoneNumber)
+                    .withEMail(eMail).build();
+
+            new Thread(() -> {
+                mDao = ContactDatabase.getDatabase(getApplicationContext()).contactDao();
+                mDao.insert(newContact);
+            }).start();
+        }
+        finish();
     }
 }
--- app/src/main/java/com/vroussea/myapplication/activities/ContactsActivity.java
+++ app/src/main/java/com/vroussea/myapplication/activities/ContactsActivity.java
@@ -1,8 +1,8 @@
 package com.vroussea.myapplication.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -10,22 +10,24 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import com.vroussea.myapplication.R;
 import com.vroussea.myapplication.adapters.ContactAdapter;
 import com.vroussea.myapplication.contact.Contact;
-import com.vroussea.myapplication.contact.ContactBuilder;
 import com.vroussea.myapplication.contact.ContactDao;
 import com.vroussea.myapplication.contact.ContactDatabase;
 
 import java.util.List;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ContactsActivity extends AppCompatActivity {
-    ContactDao mDao;
+    private ContactDao mDao;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,46 +38,21 @@ public class ContactsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
         Toolbar myToolbar = findViewById(R.id.menu_contact);
         setSupportActionBar(myToolbar);
 
-        final ListView listview = findViewById(R.id.listview);
+        displayContacts();
+    }
 
-        new Thread(() -> {
-            mDao = ContactDatabase.getDatabase(getApplicationContext()).contactDao();
+    @Override
+    protected void onRestart() {
+        super.onRestart();
 
-            Contact contact1 = ContactBuilder.aContact().withFirstName("Jean").withPhoneNumber("0103040304").build();
-            Contact contact2 = ContactBuilder.aContact().withFirstName("Jack").withPhoneNumber("0103040304").build();
-
-            mDao.insert(contact1, contact2);
-
-            final List<Contact> contacts = mDao.loadAllContacts();
-
-            final ContactAdapter adapter = new ContactAdapter(this, contacts);
-            listview.setAdapter(adapter);
-
-            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, final View view,
-                                        int position, long id) {
-                    final Contact item = (Contact) parent.getItemAtPosition(position);
-                    view.animate().alpha(0).withEndAction(new Runnable() {
-                        @Override
-                        public void run() {
-                            contacts.remove(item);
-                            new Thread(() -> {
-                                mDao.delete(item);
-                            }).start();
-                            adapter.notifyDataSetChanged();
-                            view.setAlpha(1);
-                        }
-                    });
-                }
-            });
-        }).start();
+        displayContacts();
     }
 
     @Override
@@ -90,12 +67,59 @@ public class ContactsActivity extends AppCompatActivity {
             Intent intent = new Intent(this, ContactEdit.class);
             startActivity(intent);
             return true;
-        }
-        else if (id == R.id.action_settings) {
+        } else if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private List<Contact> getContacts() throws ExecutionException, InterruptedException {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<List<Contact>> callable = new Callable<List<Contact>>() {
+            @Override
+            public List<Contact> call() {
+                mDao = ContactDatabase.getDatabase(getApplicationContext()).contactDao();
+                return mDao.loadAllContacts();
+            }
+        };
+        Future<List<Contact>> future = executor.submit(callable);
+        executor.shutdown();
+
+        return future.get();
+    }
+
+    private void displayContacts() {
+        final ListView listview = findViewById(R.id.listview);
+
+        List<Contact> contacts;
+        try {
+            contacts = getContacts();
+        } catch (Exception e) {
+            finish();
+            return;
+        }
+
+        final ContactAdapter adapter = new ContactAdapter(this, contacts);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final Contact item = (Contact) parent.getItemAtPosition(position);
+                view.animate().alpha(0).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        contacts.remove(item);
+                        new Thread(() -> {
+                            mDao.delete(item);
+                        }).start();
+                        adapter.notifyDataSetChanged();
+                        view.setAlpha(1);
+                    }
+                });
+            }
+        });
+    }
 }
